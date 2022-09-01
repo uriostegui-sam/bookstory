eab58951719c71b7778b19681bd39c1ed802a52f
Sam URIOSTEGUI <samyy.uv@gmail.com>
2022-09-01 18:24:42 +0200

    doing the general authors page

--- src/Controller/Front/AuteurController.php
+++ src/Controller/Front/AuteurController.php
@@ -5,6 +5,8 @@ namespace App\Controller\Front;
 use App\Entity\Auteur;
 use App\Repository\AuteurRepository;
 use App\Repository\CategorieRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,11 +17,24 @@ class AuteurController extends AbstractController
     /**
      * @Route("/auteurs", name="front_auteur_index", methods={"GET"})
      */
-    public function index(AuteurRepository $auteurRepository, CategorieRepository $categorieRepository): Response
-    {
+    public function index(
+        AuteurRepository $auteurRepository,
+        CategorieRepository $categorieRepository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response {
+
+        $data = $auteurRepository->findAll();
+        $auteurs = $paginator->paginate(
+            $data,
+            $request->query->getInt('page', 1),
+            8
+        );
+
+
         return $this->render('front/auteur/index.html.twig', [
             'categories' => $categorieRepository->findAll(),
-            'auteurs' => $auteurRepository->findAll(),
+            'auteurs' => $auteurs,
         ]);
     }
 
